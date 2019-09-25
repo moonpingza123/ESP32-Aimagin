4fe7c2ade9f829a9aef672813f4ff39cf3b94828
moonpingza123 <moonpingza@outlook.com>
2019-09-25 14:17:55 +0700

    up used php

--- index_home.php
+++ index_home.php
@@ -4,6 +4,12 @@
 	<title></title>
 </head>
 <body>
-
+	<?php
+	echo "<h2>PHP is Fun!</h2>";
+	echo "Hello world!<br>";
+	echo "I'm about to learn PHP!<br>";
+	echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+	?>
+	<br><span>123456789</span>
 </body>
 </html>
