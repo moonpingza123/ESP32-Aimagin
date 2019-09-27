281a80c3c6966d6f547d833f2f9d5656d0205903
moonpingza123 <moonpingza@outlook.com>
2019-09-27 16:55:28 +0700

    up DB .sql file

--- index_home.php
+++ index_home.php
@@ -6,6 +6,7 @@
 <head>
 	<title></title>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
 	<?php
@@ -40,9 +41,23 @@
 
 	<div class="row"><button onclick="buttonClicktest()">test return</button></div>
 
-<script type="text/javascript">
+	<select id="abc">
+	  <option value="0"></option>
+	  <!-- <option value="saab">Saab</option>
+	  <option value="opel">Opel</option>
+	  <option value="audi">Audi</option> -->
+	  <!-- <span id="abc">
+	  	
+	  </span> -->
+	</select>
 
-	function buttonClicktest(){
+<script >
+
+	$(document).ready(function () {
+    //     var html ='<option value="abc">abd</option>';
+	  	// // html += '<option value="abc">abd</option>';
+    //     $("#abc").append(html); 
+
         <?php 
 
         	$sql = "SELECT BibleID, BibleTHName, BibleTHInitialsName FROM bible";
@@ -53,10 +68,16 @@
 			    while($row = $result->fetch_assoc()) {
 			        // echo "id: " . $row["BibleID"]. " - Name: " . $row["BibleTHName"]. " " . $row["BibleTHInitialsName"]. "<br>";
 			        ?>
-			        var ex1 = "<?php echo $row["BibleTHName"] ?>";
-			        console.log(ex1);
+			        var ex1 = '<?php echo $row["BibleID"] ?>';
+			        var ex2 = '<?php echo $row["BibleTHInitialsName"] ?>';
+			        // console.log(ex2);
+
+			        // var html = '<option value="abc">abd</option>';
+			        var html = '<option value="'+ ex1 +'">'+ ex2 +'</option>';
+			        $("#abc").append(html);
+
 			       	var ex3 = "<?php echo $str ?>";
-			        console.log(ex3);
+			        // console.log(ex3);
 			        // console.log(ex3);
 
 			        <?php
@@ -68,10 +89,45 @@
 			$conn->close();
         ?>
 
+        // $("#abc").append(html);
+
+    	console.log(123456789);
+    });
+
+	function buttonClicktest(){
+		// var html ='';
+   //      <?php 
+
+   //      	$sql = "SELECT BibleID, BibleTHName, BibleTHInitialsName FROM bible";
+			// $result = $conn->query($sql);
+
+			// if ($result->num_rows > 0) {
+			//     // output data of each row
+			//     while($row = $result->fetch_assoc()) {
+			//         // echo "id: " . $row["BibleID"]. " - Name: " . $row["BibleTHName"]. " " . $row["BibleTHInitialsName"]. "<br>";
+			//         ?>
+			//         var ex1 = '<?php echo $row["BibleID"] ?>';
+			//         var ex2 = '<?php echo $row["BibleTHInitialsName"] ?>';
+			//         console.log(ex2);
+
+			//         // html += '<option value="abc">abd</option>';
+
+			//        	var ex3 = "<?php echo $str ?>";
+			//         // console.log(ex3);
+			//         <?php
+			//     }
+			// } else {
+			//     echo "0 results";
+			// }
+
+			// $conn->close();
+   //      ?>
    		// var ex3 = "<?php echo $str ?>";
    		// console.log(ex3);
 
     }
+
+
 
 </script>
 <br><span>123456</span>
